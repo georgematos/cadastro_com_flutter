1a8e4cf01132a38721a9a9a3d1c0402de9684a38
georgematos <georgemattos@gmail.com>
2020-08-01 05:47:56 -0300

    creating a ListView - time stop at: 00:12:37

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/data/dummy_users.dart';
 
 class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const users = dummy_users;
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Usuários'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) =>
+            Text(users.values.elementAt(index).name),
       ),
     );
   }
