4bfe6291e63116a0daa5a89bafe1c61d907b9ae2
georgematos <georgemattos@gmail.com>
2020-08-04 06:50:51 -0300

    adding add, edit and delete buttons

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -12,7 +12,25 @@ class UserTile extends StatelessWidget {
         ? CircleAvatar(child: Icon(Icons.person))
         : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl));
     return ListTile(
-      leading: avatar,
-    );
+        leading: avatar,
+        title: Text(user.name),
+        subtitle: Text(user.email),
+        trailing: Container(
+          width: 100,
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.edit),
+                color: Colors.orange,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.delete),
+                color: Colors.red,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ));
   }
 }
