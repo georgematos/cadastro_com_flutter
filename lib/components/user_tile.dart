9454c3671eecd61ea051ca04f79a0de7f8883ef3
georgematos <georgemattos@gmail.com>
2020-08-08 15:01:36 -0300

    mostrando alerta e excluindo usuario

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users_provider.dart';
 import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
-
   const UserTile(this.user);
 
   @override
@@ -13,30 +14,66 @@ class UserTile extends StatelessWidget {
         ? CircleAvatar(child: Icon(Icons.person))
         : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl));
     return ListTile(
-        leading: avatar,
-        title: Text(user.name),
-        subtitle: Text(user.email),
-        trailing: Container(
-          width: 100,
-          child: Row(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.edit),
-                color: Colors.orange,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(
-                    AppRoutes.user_form,
-                    arguments: user,
-                  );
-                },
-              ),
-              IconButton(
+      leading: avatar,
+      title: Text(user.name),
+      subtitle: Text(user.email),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.user_form,
+                  arguments: user,
+                );
+              },
+            ),
+            IconButton(
                 icon: Icon(Icons.delete),
                 color: Colors.red,
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ));
+                onPressed: () {
+                  showAlertDialog(context);
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+
+  showAlertDialog(BuildContext context) {
+    final UsersProvider users = Provider.of(context, listen: false);
+
+    Widget cancelButton = FlatButton(
+      child: Text('Cancelar'),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+
+    Widget confirmButton = FlatButton(
+      child: Text('Confirmar'),
+      onPressed: () {
+        users.remove(user.id);
+        Navigator.of(context).pop();
+      },
+    );
+
+    AlertDialog alert = AlertDialog(
+      title: Text('Alerta'),
+      content: Text('Tem certeza que deseja excluir?'),
+      actions: <Widget>[
+        cancelButton,
+        confirmButton,
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        });
   }
 }
